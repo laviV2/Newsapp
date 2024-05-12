6c028822e1db16d87991adb9acca57a66b96c03e
laviV2 <167812176+laviV2@users.noreply.github.com>
2024-05-12 17:31:10 +0530

    Add files via upload

---
+++ science.js
@@ -0,0 +1,35 @@
+const main=document.getElementById('articles-container');
+
+let data;
+let datalength=0;
+
+    async function getData(){
+     const res = await fetch("https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=d90e5439d81b428a80ad09f9a694c9f3");
+     const result= await res.json();
+     data = result.articles;
+     datalength=data.length;
+     console.log("\n:data:",data);
+
+     renderUI();
+    }
+
+getData();
+
+  
+function renderUI(){
+for(let i=0; i<datalength; i++){
+const article=document.createElement('article');
+article.innerHTML=`
+            <img class="article-image" src="${data[i].urlToImage}" alt="news Image 1"/> 
+             <div class="article-content">
+                <h2 id="heading-1">${data[i].title}</h2>
+            <p>
+               ${data[i].description}
+            </p>
+            <a href="${data[i].url}" target="_blank">Read More....</a>
+            <p>~"${data[i].author}"
+            </div>
+                `;  
+    main.appendChild(article);
+}
+}
